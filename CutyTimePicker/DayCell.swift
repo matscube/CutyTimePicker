4ef2c97eac17aa828ffdfa3a0f39cb3e7289d555
Takanori Matsumoto <matscube@gmail.com>
2015-02-12 09:00:35 +0900

    Override init.

--- CutyTimePicker/DayCell.swift
+++ CutyTimePicker/DayCell.swift
@@ -9,5 +9,11 @@
 import UIKit
 
 class DayCell: UICollectionViewCell {
-    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
